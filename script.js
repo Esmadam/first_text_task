1d09968b51e5871c5d525c7e60812bd34012058f
Esmadam <esmurziev.fortech@gmail.com>
2023-02-13 15:06:11 +0300

    logical part

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("asd")
+const select =  document.querySelector('#choose');
+const option = document.querySelectorAll('select > option');
+
+
+const input = document.querySelector('input');
+const button = document.querySelector('.button');
+
+
+select.addEventListener('click', () => {
+    const value = select.value;
+    let text = select.options[select.selectedIndex].text;
+
+    input.value = text;
+});
+
+button.addEventListener('click', () => {
+    let selected = select.options[select.selectedIndex];
+    selected.text = input.value;
+    
+})
